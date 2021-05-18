c0338ff0de59972322914e3c71976dcd4cf93b99
David Rudolf <84070566+Rudi19881988@users.noreply.github.com>
2021-05-18 16:56:16 +0200

    Beispiel 2

--- challenge_01.js
+++ challenge_01.js
@@ -9,7 +9,8 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 ‘Die Summe der Zahlen ist: summe ‘
 */
 
-
+//Beispiel 1
+/*
 let zahl1, zahl2
 let Summe
 zahl1 = prompt("Bitte gebe Zahl1 ein");
@@ -19,3 +20,17 @@ zahl1 = 15;
 zahl2 = 29;
 
 console.log(zahl1 + zahl2)
+*/
+
+//Beispiel 2
+/*
+let zahl1, zahl2
+let Summe
+zahl1 = parseFloat(prompt("Bitte gebe Zahl1 ein"));
+zahl2 = parseFloat(prompt("Bitte gebe Zahl2 ein"));
+
+summe = zahl1 + zahl2
+
+      document.write(" Das Ergebnis lautet " + summe );
+console.log(zahl1 + zahl2);
+*/
